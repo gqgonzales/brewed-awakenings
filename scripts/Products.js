f4bd19ba7cba95311dd916f86af4493567f878df
Gabe Gonzales <gq.gonzales1@gmail.com>
2021-04-22 13:30:15 -0500

    i give up

--- scripts/Products.js
+++ scripts/Products.js
@@ -1,5 +1,25 @@
 import { getProducts } from "./database.js";
 
+// Viewing Products on Click (adapted from DeShawn Dog Walking)
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("walker")) {
+            const [,walkerId] = itemClicked.id.split("--")
+  
+            for (const walker of walkers) {
+                if (walker.id === parseInt(walkerId)) {
+                    window.alert(`${walker.name} costs ${product.price}`)
+                }
+            }
+        }
+    }
+  )
+
+  // Original code below
+
 const products = getProducts();
 
 export const Products = () => {
